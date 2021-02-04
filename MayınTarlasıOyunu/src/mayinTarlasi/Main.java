23294021d0533458b6020e580ae19488425ef925
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2021-02-04 23:26:24 +0300

    Add files via upload

---
+++ MayınTarlasıOyunu/src/mayinTarlasi/Main.java
@@ -0,0 +1,9 @@
+package mayinTarlasi;
+public class Main {
+    public static void main (String[] args)
+    {
+        MayinTarlasi mayinTarlasi = new MayinTarlasi(5,5);
+        mayinTarlasi.baslat();
+        
+    }
+}
