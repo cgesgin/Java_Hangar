2f5e4c7e509d58ae843216516e838ae7af02849e
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2021-03-24 00:41:29 +0300

    Delete MayınTarlasıOyunu directory

--- MayınTarlasıOyunu/src/mayinTarlasi/Main.java
+++
@@ -1,9 +0,0 @@
-package mayinTarlasi;
-public class Main {
-    public static void main (String[] args)
-    {
-        MayinTarlasi mayinTarlasi = new MayinTarlasi(5,5);
-        mayinTarlasi.baslat();
-        
-    }
-}
