fd3f409453f831f906b02dab1c283ae7c1009df8
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2021-03-24 08:29:40 +0300

    Delete OOP_macera_oyunu directory

--- OOP_macera_oyunu/src/oop_Survivor_game/Magara.java
+++
@@ -1,9 +0,0 @@
-
-package oop_Survivor_game;
-
-public class Magara extends SavasBölge{
-
-     Magara(Oyuncu oyuncu) {
-        super(oyuncu, "Magara", new Ayı(),"Yemek");
-    }
-}
--- OOP_macera_oyunu/src/oop_Survivor_game/Zombi.java
+++
@@ -1,9 +0,0 @@
-
-package oop_Survivor_game;
-
-public class Zombi extends Canavar {
-
-    public Zombi() {
-        super("Zombi", 3, 10, 4, 3);
-    }
-}
