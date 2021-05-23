a23ffed83188e082aaa61bdc841db7bf0b542d35
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2021-05-23 17:06:12 +0300

    Update OrnekKullanim.java

--- Java_ile_Benzersiz_Sayi_üretme/OrnekKullanim.java
+++ Java_ile_Benzersiz_Sayi_üretme/OrnekKullanim.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Main {
+public class OrnekKullanim {
 
     public static void main(String[] args) throws IOException {
 
